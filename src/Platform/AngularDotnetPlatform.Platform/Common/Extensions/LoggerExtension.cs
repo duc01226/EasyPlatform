46cbacfe3832a503a0778b2b6bc4076938e92d09
Duc Dang <duc01226@gmail.com>
2022-01-16 07:11:15 +0700

    Refactor logging

--- src/Platform/AngularDotnetPlatform.Platform/Common/Extensions/LoggerExtension.cs
+++ src/Platform/AngularDotnetPlatform.Platform/Common/Extensions/LoggerExtension.cs
@@ -9,42 +9,42 @@ namespace AngularDotnetPlatform.Platform.Common.Extensions
 {
     public static class LoggerExtension
     {
-        public static void LogInformationIfEnabled(this ILogger logger, Exception exception, string message, params object[] args)
+        public static void LogInformationIfEnabled(this ILogger logger, Exception exception, string message)
         {
-            logger.LogIfEnabled(LogLevel.Information, exception, message, args);
+            logger.LogIfEnabled(LogLevel.Information, exception, message);
         }
 
-        public static void LogInformationIfEnabled(this ILogger logger, string message, params object[] args)
+        public static void LogInformationIfEnabled(this ILogger logger, string message)
         {
-            logger.LogIfEnabled(LogLevel.Information, message, args);
+            logger.LogIfEnabled(LogLevel.Information, message);
         }
 
-        public static void LogInformationIfEnabled(this ILogger logger, EventId eventId, string message, params object[] args)
+        public static void LogInformationIfEnabled(this ILogger logger, EventId eventId, string message)
         {
-            logger.LogIfEnabled(LogLevel.Information, eventId, message, args);
+            logger.LogIfEnabled(LogLevel.Information, eventId, message);
         }
 
-        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, Exception exception, string message, params object[] args)
+        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, Exception exception, string message)
         {
             if (logger.IsEnabled(logLevel))
             {
-                logger.Log(logLevel, exception, message, args);
+                logger.Log(logLevel, exception, message);
             }
         }
 
-        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, string message, params object[] args)
+        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, string message)
         {
             if (logger.IsEnabled(logLevel))
             {
-                logger.Log(logLevel, message, args);
+                logger.Log(logLevel, message);
             }
         }
 
-        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, EventId eventId, string message, params object[] args)
+        public static void LogIfEnabled(this ILogger logger, LogLevel logLevel, EventId eventId, string message)
         {
             if (logger.IsEnabled(logLevel))
             {
-                logger.Log(logLevel, eventId, message, args);
+                logger.Log(logLevel, eventId, message);
             }
         }
     }
