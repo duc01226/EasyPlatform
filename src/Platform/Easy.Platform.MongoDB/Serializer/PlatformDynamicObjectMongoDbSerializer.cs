888337fe09b69b2323b557f5535740f60352fcff
Duc Dang <duc01226@gmail.com>
2022-04-27 05:47:59 +0700

    Add mongo serializer for object

--- src/Platform/Easy.Platform.MongoDB/Serializer/PlatformDynamicObjectMongoDbSerializer.cs
+++ src/Platform/Easy.Platform.MongoDB/Serializer/PlatformDynamicObjectMongoDbSerializer.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Easy.Platform.Common.JsonSerialization;
 using Easy.Platform.MongoDB.Serializer.Abstract;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 
@@ -14,11 +15,99 @@ namespace Easy.Platform.MongoDB.Serializer
     {
         public override object Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
         {
+            return TryGetReflectionDynamic(BsonSerializer.Deserialize<BsonValue>(context.Reader));
         }
 
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
         {
             BsonSerializer.Serialize(context.Writer, PlatformObjectJsonConverter.TryGetReflectionDynamicIfJsonElement(value));
+        }
+
+        private static dynamic TryGetReflectionDynamic(BsonValue dynamicObjectAsBsonValue)
+        {
+            dynamic result = null;
+            switch (dynamicObjectAsBsonValue.BsonType)
+            {
+                case BsonType.Int32:
+                {
+                    result = dynamicObjectAsBsonValue.AsInt32;
+                    break;
+                }
+
+                case BsonType.Int64:
+                {
+                    result = dynamicObjectAsBsonValue.AsInt64;
+                    break;
+                }
+
+                case BsonType.Double:
+                {
+                    result = dynamicObjectAsBsonValue.AsDouble;
+                    break;
+                }
+
+                case BsonType.Decimal128:
+                {
+                    result = dynamicObjectAsBsonValue.AsDecimal128;
+                    break;
+                }
+
+                case BsonType.DateTime:
+                {
+                    result = dynamicObjectAsBsonValue.ToUniversalTime();
+                    break;
+                }
+
+                case BsonType.Boolean:
+                    result = dynamicObjectAsBsonValue.AsBoolean;
+                    break;
+                case BsonType.String:
+                {
+                    try
+                    {
+                        result = dynamicObjectAsBsonValue.AsString;
+                        if (DateTimeOffset.TryParse(result, out DateTimeOffset dateTimeOffsetValue))
+                        {
+                            result = dateTimeOffsetValue;
+                        }
+                        else if (DateTime.TryParse(result, out DateTime dateValue))
+                        {
+                            result = dateValue;
+                        }
+                    }
+                    catch
+                    {
+                        result = dynamicObjectAsBsonValue.AsString;
+                    }
+
+                    break;
+                }
+
+                case BsonType.Array:
+                    result = dynamicObjectAsBsonValue
+                        .AsBsonArray
+                        .Select(o => TryGetReflectionDynamic(o))
+                        .ToArray();
+                    break;
+
+                case BsonType.Document:
+                {
+                    var dynamicDocument = dynamicObjectAsBsonValue.AsBsonDocument;
+                    var keyValueObject = dynamicDocument.ToDictionary(item => item.Name, item => item.Value);
+                    result = keyValueObject;
+                    break;
+                }
+
+                case BsonType.Undefined:
+                case BsonType.Null:
+                default:
+                {
+                    result = null;
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
